ee73df995112a1e5ec7261a074a2bbdd8dd369dd
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-24 10:49:44 -0700

    set up server connection to databse and orm files

---
+++ models/burger.js
@@ -0,0 +1,15 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function (cb) {
+
+  },
+  insert: function (cb) {
+
+  },
+  update: function (cb) {
+
+  }
+}
+
+module.exports = burger;
